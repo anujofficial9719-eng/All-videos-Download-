3a19aee2eb8eaa23874423bd603d170991a491cd
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-22 23:16:39 +0530

    Update check_user_status.py

--- plugins/check_user_status.py
+++ plugins/check_user_status.py
@@ -21,6 +21,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You R Banned!.. Contact @SnowBall_Official 😝", quote=True)
+            await cmd.reply_text("You R Banned!.. Contact @anujedits76😝", quote=True)
             return
     await cmd.continue_propagation()
